c97d1b0f5a6a56a50c96728d18f5539a5bedc404
wonjunLee123 <opeveben@gmail.com>
2025-05-03 12:37:23 +0900

    I키를 눌러 활성화

---
+++ Assets/Script/inven.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class ClickEventTest : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    bool activeInventory = false;
+    private void Start()
+    {
+        inventoryPanel.SetActive(activeInventory);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("인벤토리 활성화");
+            activeInventory = !activeInventory;
+            inventoryPanel.SetActive(activeInventory);
+        }
+    }
+}
+
+
