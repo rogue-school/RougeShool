3909dcc2a2b480f6734839e83ac9764492dd4f0d
wonjunLee123 <opeveben@gmail.com>
2025-05-09 20:12:12 +0900

    애니메이션 추가

--- Assets/Script/inven.cs
+++ Assets/Script/inven.cs
@@ -1,31 +1,69 @@
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
+using UnityEngine.UI;
+using System.Collections;
 
-public class ClickEventTest : MonoBehaviour
+public class InventoryAnimator : MonoBehaviour
 {
-    public GameObject inventoryPanel;
-    bool activeInventory = false;
+    public RectTransform inventoryPanel;
+    public Button bagButton;
+    public Button closeButton;
+
+    public float animationDuration = 0.3f;
+    public Vector2 closedPosition = new Vector2(300f, 0f); // 오른쪽 밖
+    public Vector2 openPosition = new Vector2(0f, 0f);      // 오른쪽에 붙게
+
+    private bool isOpen = false;
+    private Coroutine animationCoroutine;
+
     private void Start()
     {
-        inventoryPanel.SetActive(activeInventory);
+        inventoryPanel.gameObject.SetActive(false); // 처음엔 안 보이게
+        closeButton.onClick.AddListener(CloseInventory);
+        bagButton.onClick.AddListener(OpenInventory);
     }
 
-   
-    void Update()
+    private void OpenInventory()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (isOpen) return;
+        isOpen = true;
+
+        inventoryPanel.gameObject.SetActive(true); // 열기 전에 활성화
+        inventoryPanel.anchoredPosition = closedPosition;
+
+        AnimatePanel(openPosition);
+    }
+
+    private void CloseInventory()
+    {
+        if (!isOpen) return;
+        isOpen = false;
+
+        AnimatePanel(closedPosition, () => {
+            inventoryPanel.gameObject.SetActive(false); // 닫고 나서 비활성화
+        });
+    }
+
+    private void AnimatePanel(Vector2 targetPosition, System.Action onComplete = null)
+    {
+        if (animationCoroutine != null)
+            StopCoroutine(animationCoroutine);
+        animationCoroutine = StartCoroutine(SmoothMove(targetPosition, onComplete));
+    }
+
+    private IEnumerator SmoothMove(Vector2 targetPosition, System.Action onComplete)
+    {
+        Vector2 startPos = inventoryPanel.anchoredPosition;
+        float timer = 0f;
+
+        while (timer < animationDuration)
         {
-            Debug.Log("클릭해서 인벤 활성화.");
-
+            timer += Time.deltaTime;
+            float t = timer / animationDuration;
+            inventoryPanel.anchoredPosition = Vector2.Lerp(startPos, targetPosition, t);
+            yield return null;
         }
-    }
 
-    public void OnButtonClicked()
-    {
-        Debug.Log("인벤토리 활성화");
-        activeInventory = !activeInventory;
-        inventoryPanel.SetActive(activeInventory);
+        inventoryPanel.anchoredPosition = targetPosition;
+        onComplete?.Invoke();
     }
 }
-
-
