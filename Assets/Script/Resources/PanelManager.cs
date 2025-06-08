a7cbcdf9966ea9ce58e69c8d0d2890003a86ef29
leejeasung <hjk737900@gmail.com>
2025-06-08 16:43:40 +0900

    save

--- Assets/Script/Resources/PanelManager.cs
+++ Assets/Script/Resources/PanelManager.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 public class PanelManager : MonoBehaviour
 {
     public GameObject panelA;
-   
 
-    // 1. 처음 버튼 누르면 PanelA 활성화
+    // 비활성화할 오브젝트들 추가
+    public GameObject panelToDisable;      // DeathPanel > Panel
+    public GameObject defeatTextToDisable; // DeathPanel > DefeatText
+
+    // 버튼을 눌렀을 때 실행
     public void ShowPanelA()
     {
         panelA.SetActive(true);
-    }
 
-  
+        if (panelToDisable != null)
+            panelToDisable.SetActive(false);
+
+        if (defeatTextToDisable != null)
+            defeatTextToDisable.SetActive(false);
+    }
 }
