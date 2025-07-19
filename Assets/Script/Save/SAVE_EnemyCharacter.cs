39a5bd70a969f62508480ba7a239d432d1d8da21
wonjunLee123 <opeveben@gmail.com>
2025-07-19 19:19:15 +0900

    SAVE 일단 올리기

---
+++ Assets/Script/Save/SAVE_EnemyCharacter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SAVE_EnemyCharacter
+{
+    public string Name;
+    public int Level;
+    public int HP;
+
+    public SAVE_EnemyCharacter(string name, int level, int hp)
+    {
+        Name = name;
+        Level = level;
+        HP = hp;
+    }
+}
