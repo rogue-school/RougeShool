344119de0914ec1d74af833c49ab15f17dd2e911
wonjunLee123 <opeveben@gmail.com>
2025-08-23 19:24:37 +0900

    바꾸고

--- Assets/Script/Save/New Folder/SaveModels.cs
+++ Assets/Script/Save/New Folder/SaveModels.cs
@@ -1,16 +1,25 @@
-using UnityEngine;
+using System;
 
-public class SaveModels : MonoBehaviour
+namespace Game.Save
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// 적 1명의 저장 상태
+    /// </summary>
+    [Serializable]
+    public class EnemyState
     {
-        
+        public string enemyId;     // ScriptableObject의 name (고유 ID 용도)
+        public int currentHp;
+        public bool isDead;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 전투(배틀) 씬에서 저장할 전체 데이터: 플레이어 HP + 적들 상태
+    /// </summary>
+    [Serializable]
+    public class BattleSaveData
     {
-        
+        public int playerHp;
+        public EnemyState[] enemies;
     }
 }
--- Assets/Script/Save/New Folder/SaveSystem.cs
+++ Assets/Script/Save/New Folder/SaveSystem.cs
@@ -1,16 +1,46 @@
+using System.IO;
 using UnityEngine;
 
-public class SaveSystem : MonoBehaviour
+namespace Game.Save
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// JSON 저장/불러오기 유틸
+    /// </summary>
+    public static class SaveSystem
     {
-        
-    }
+        private static readonly string SavePath =
+            Path.Combine(Application.persistentDataPath, "battle_save.json");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public static void Save(BattleSaveData data)
+        {
+            var json = JsonUtility.ToJson(data, prettyPrint: true);
+            File.WriteAllText(SavePath, json);
+            Debug.Log($"[SaveSystem] 저장 완료: {SavePath}");
+        }
+
+        public static BattleSaveData Load()
+        {
+            if (!File.Exists(SavePath))
+            {
+                Debug.LogWarning("[SaveSystem] 저장 파일이 없습니다.");
+                return null;
+            }
+
+            var json = File.ReadAllText(SavePath);
+            var data = JsonUtility.FromJson<BattleSaveData>(json);
+            Debug.Log($"[SaveSystem] 불러오기 완료: {SavePath}");
+            return data;
+        }
+
+        public static bool Exists() => File.Exists(SavePath);
+
+        public static void Delete()
+        {
+            if (File.Exists(SavePath))
+            {
+                File.Delete(SavePath);
+                Debug.Log("[SaveSystem] 저장 파일 삭제");
+            }
+        }
     }
 }
