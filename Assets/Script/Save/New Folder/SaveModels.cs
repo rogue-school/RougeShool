6fd3c086202dce1b05b67ae2969e7b1a176763bf
wonjunLee123 <opeveben@gmail.com>
2025-08-24 14:55:59 +0900

    ㅁㄴㅇㄹ

--- Assets/Script/Save/New Folder/SaveModels.cs
+++ Assets/Script/Save/New Folder/SaveModels.cs
@@ -1,21 +1,16 @@
+// SaveModels.cs
 using System;
 
 namespace Game.Save
 {
-    /// <summary>
-    /// 적 1명의 저장 상태
-    /// </summary>
     [Serializable]
     public class EnemyState
     {
-        public string enemyId;     // ScriptableObject의 name (고유 ID 용도)
+        public string enemyId;
         public int currentHp;
         public bool isDead;
     }
 
-    /// <summary>
-    /// 전투(배틀) 씬에서 저장할 전체 데이터: 플레이어 HP + 적들 상태
-    /// </summary>
     [Serializable]
     public class BattleSaveData
     {
